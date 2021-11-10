848353628897e6f6df77abc1618818bdeebf5d4c
liggia <liggia.cruz@gmail.com>
2021-11-10 11:57:36 -0300

    fix: getProductsByCategory

--- src/controllers/homePosts.js
+++ src/controllers/homePosts.js
@@ -3,7 +3,7 @@ import connection from '../database/database.js';
 async function getMainCategories(req, res) {
   try {
     const categories = await connection.query(`
-          SELECT * FROM categories LIMIT 6;
+        SELECT * FROM categories ORDER BY id LIMIT 6;
       `);
     res.send(categories.rows);
   } catch (e) {
@@ -20,9 +20,29 @@ async function getProductsByCategory(req, res) {
                 FROM products 
             JOIN products_categories 
                 ON products.id = products_categories.product_id 
-            WHERE products_categories.category_id = $1;
+            WHERE products_categories.category_id = $1 ORDER BY id LIMIT 16;
         `, [id]);
-    res.send(products.rows);
+
+    const images = await connection.query(`
+            SELECT images.*, products_images.product_id FROM images JOIN products_images ON images.id = products_images.image_id;
+            `);
+
+    const newArray = products.rows.map((product) => {
+      const imagesProduct = [];
+      images.rows.forEach((img) => {
+        if (product.id === img.product_id) {
+          imagesProduct.push(img);
+        }
+      });
+      return (
+        {
+          ...product,
+          images: imagesProduct,
+        }
+      );
+    });
+
+    res.send(newArray);
   } catch (e) {
     res.sendStatus(500);
   }
