d4b8be8588e6f1badfb368ae5e506c656255eba9
liggia <liggia.cruz@gmail.com>
2021-11-12 16:12:14 -0300

    feat: getProductsById add colors and sizes

--- src/controllers/homePosts.js
+++ src/controllers/homePosts.js
@@ -27,23 +27,21 @@ async function getProductsByCategory(req, res) {
             WHERE products_categories.category_id = $1 ORDER BY id LIMIT 16;
         `, [id]);
 
-    const images = await connection.query(`
-            SELECT images.*, products_images.product_id FROM images JOIN products_images ON images.id = products_images.image_id;
-            `);
+    const images = await connection.query('SELECT images.*, products_images.product_id FROM images JOIN products_images ON images.id = products_images.image_id;');
+    const colors = await connection.query('SELECT colors.*, products_colors.product_id FROM colors JOIN products_colors ON colors.id = products_colors.color_id;');
+    const sizes = await connection.query('SELECT sizes.*, products_size.product_id FROM sizes JOIN products_size ON sizes.id = products_size.size_id;');
 
     const newArray = products.rows.map((product) => {
-      const imagesProduct = [];
-      images.rows.forEach((img) => {
-        if (product.id === img.product_id) {
-          imagesProduct.push(img);
-        }
+      const newimages = images.rows.filter((image) => image.product_id === product.id);
+      const newcolors = colors.rows.filter((color) => color.product_id === product.id);
+      const newsizes = sizes.rows.filter((size) => size.product_id === product.id);
+
+      return ({
+        ...product,
+        images: newimages,
+        colors: newcolors,
+        sizes: newsizes,
       });
-      return (
-        {
-          ...product,
-          images: imagesProduct,
-        }
-      );
     });
 
     res.send(newArray);
